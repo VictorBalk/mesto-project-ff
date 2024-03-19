21a560c567e29f477159f3394ca87982418ed549
Hio <balkviktor@gmail.com>
2024-03-20 01:48:55 +0300

    Первый Коммит

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,32 @@
 const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+  {
+    name: "Якутия",
+    link: "./images/cards/Yakutia.jpg",
+    alt: "Дом шамана на поле в Якутии"
+  },
+  {
+    name: "Пермский край",
+    link: "./images/cards/Kungur_Ice_Cave.jpg",
+    alt: "Кунгурская ледяная пещера, Пермский край"
+  },
+  {
+    name: "Курильские острова",
+    link: "./images/cards/KurileIslands.jpg",
+    alt: "Вулкан Креницына, Курильские острова"
+  },
+  {
+    name: "Москва",
+    link: "./images/cards/Moscow.jpg",
+    alt: "Стекляные небоскребы, Москва"
+  },
+  {
+    name: "Забайкалье",
+    link: "./images/cards/Zabaykalye.jpg",
+    alt: "Чарские пески, Забайкалье"
+  },
+  {
+    name: "Санкт-Петербург",
+    link: "./images/cards/SaintPetersburg.jpg",
+    alt: "Исаакиевский собор, Санкт-Петербург"
+  }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,42 @@
 // @todo: Темплейт карточки
+const oTeemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const oContainerCard = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function onAddCard(oCard) {
+    const oCardElement = oTeemplate.querySelector(".places__item").cloneNode(true);
+
+    const oImg = oCardElement.querySelector(".card__image");
+
+    oImg.setAttribute('src', oCard.link);
+    oImg.setAttribute('alt', oCard.alt);
+
+    const oDeleteButton = oCardElement.querySelector(".card__delete-button");
+    oDeleteButton.addEventListener('click', onRemoveCard);
+
+    const oTitle = oCardElement.querySelector(".card__title");
+    oTitle.textContent = oCard.name;
+
+    const oLikeButton = oCardElement.querySelector(".card__like-button");
+    oLikeButton.addEventListener('click', onLike);
+
+    oContainerCard.append(oCardElement);
+}
 
 // @todo: Функция удаления карточки
+function onRemoveCard(oEvent) {
+
+    oEvent.target.closest(".places__item").remove();
+
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+    onAddCard(item);
+});
+
+function onLike(oEvent) {
+    // alert("Вы нажали на кнопку лайк");
+}
