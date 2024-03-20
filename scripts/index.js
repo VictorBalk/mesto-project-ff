647e4118d66a1b8cebaf2538e88aefb7673ec08f
Hio <balkviktor@gmail.com>
2024-03-20 10:29:19 +0300

    Исправления после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const oTeemplate = document.querySelector("#card-template").content;
 const oContainerCard = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function onAddCard(oCard) {
+function createCard(oCard, removefunction) {
     const oCardElement = oTeemplate.querySelector(".places__item").cloneNode(true);
 
     const oImg = oCardElement.querySelector(".card__image");
@@ -14,7 +14,7 @@ function onAddCard(oCard) {
     oImg.setAttribute('alt', oCard.alt);
 
     const oDeleteButton = oCardElement.querySelector(".card__delete-button");
-    oDeleteButton.addEventListener('click', onRemoveCard);
+    oDeleteButton.addEventListener('click', removefunction);
 
     const oTitle = oCardElement.querySelector(".card__title");
     oTitle.textContent = oCard.name;
@@ -22,19 +22,22 @@ function onAddCard(oCard) {
     const oLikeButton = oCardElement.querySelector(".card__like-button");
     oLikeButton.addEventListener('click', onLike);
 
-    oContainerCard.append(oCardElement);
+    return oCardElement;
 }
+
+function renderCard(oCard, removefunction) {
+    oContainerCard.append(createCard(oCard, removefunction));
+}
+
 
 // @todo: Функция удаления карточки
 function onRemoveCard(oEvent) {
-
     oEvent.target.closest(".places__item").remove();
-
 }
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach((item) => {
-    onAddCard(item);
+    renderCard(item, onRemoveCard);
 });
 
 function onLike(oEvent) {
